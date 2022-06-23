4fd1964e08bb7873822c3357a52fe9e6b91952d4
DanielNasc <danielnasc15987@gmail.com>
2022-06-23 00:09:07 -0300

    AuthContext

---
+++ src/pages/index.tsx
@@ -0,0 +1,34 @@
+import { FormEvent, useContext, useState } from "react";
+import { AuthContext } from "../contexts/AuthContext";
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const { isAuthenticated, signIn } = useContext(AuthContext);
+
+  async function handleSubmit(e: FormEvent) {
+    e.preventDefault();
+
+    await signIn({ email, password });
+
+    setEmail("");
+  }
+
+  return (
+    <div className={styles.container} onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button type="submit">Entrar</button>
+    </div>
+  );
+}
